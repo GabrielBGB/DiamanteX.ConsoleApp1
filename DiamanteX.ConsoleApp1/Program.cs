0c7841944730fa325aa77424692508afd055f1af
GabrielBGB <gabrielprogramacao@outlook.com.br>
2025-04-09 14:44:37 -0300

    Feito a finalização e teste do codigo e não foi encontrado nenhum erro ou bug nesta versão

--- DiamanteX.ConsoleApp1/Program.cs
+++ DiamanteX.ConsoleApp1/Program.cs
@@ -8,15 +8,15 @@
 
             while (repetir)
             {
-                #region //Entrada e Validação
+                #region Entrada e Validação
                 int numero = ObterNumeroImpar();
                 #endregion
 
-                #region //Geração do Diamante
+                #region Geração do Diamante
                 DesenharDiamante(numero);
                 #endregion
 
-                #region //Pergunta para repetir
+                #region Pergunta para repetir
                 Console.Write("\nDeseja executar novamente? (s/n): ");
                 string resposta = Console.ReadLine();
 
@@ -24,15 +24,18 @@
                 Console.WriteLine();
                 #endregion
             }
+
             Console.WriteLine("Encerrando o programa. Até logo!");
         }
-        #region //Função: Obter número ímpar válido 
+
+        #region Função: Obter número ímpar válido
         static int ObterNumeroImpar()
         {
             int numero;
+
             while (true)
             {
-                Console.Write("Digite um número ímpar");
+                Console.Write("Digite um número ímpar: ");
                 string entrada = Console.ReadLine();
 
                 if (int.TryParse(entrada, out numero) && numero % 2 != 0)
@@ -42,31 +45,39 @@
                 else
                 {
                     Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro ímpar.");
-
                 }
             }
         }
         #endregion
 
-        #region //Função: Desenhar Diamante 
+        #region Função: Desenhar diamante
         static void DesenharDiamante(int tamanho)
         {
             int meio = tamanho / 2;
 
-            //Parte superior (inclui linha do meio)
-            for (int i = 0; i < meio; i++)
-            {
-                Imprimirlinha(tamanho, i);
-
-            }
-            //Parte Inferior
-            for (int i = meio - 1; i >= 0; i--)
+            // Parte superior (inclui linha do meio)
+            for (int i = 0; i <= meio; i++)
             {
                 ImprimirLinha(tamanho, i);
             }
 
+            // Parte inferior
+            for (int i = meio - 1; i >= 0; i--)
+            {
+                ImprimirLinha(tamanho, i);
+            }
         }
         #endregion
 
         #region Função: Imprimir linha do diamante
+        static void ImprimirLinha(int tamanho, int linhaAtual)
+        {
+            int qtdEspacos = (tamanho / 2) - linhaAtual;
+            int qtdXs = linhaAtual * 2 + 1;
+
+            Console.Write(new string(' ', qtdEspacos));
+            Console.WriteLine(new string('X', qtdXs));
+        }
+        #endregion
     }
+}

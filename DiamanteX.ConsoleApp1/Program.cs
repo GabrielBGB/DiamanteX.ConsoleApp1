114d6dc154bef9d3dfc1bbc49e634d2721a8ae14
gabri <gabri@GB>
2025-04-09 09:41:20 -0300

    Adicionar arquivos de projeto.

---
+++ DiamanteX.ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DiamanteX.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repetir = true;
+
+            while (repetir)
+            {
+            }
+}
